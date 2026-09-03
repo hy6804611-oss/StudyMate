671dd6238bc283e6d675a154d1ef4e7e93d92b0e
hy6804611-oss <hy6804611@gmail.com>
2026-09-03 17:58:07 +0100

    Fix Android back navigation

--- android/app/src/main/java/com/studymate/app/MainActivity.java
+++ android/app/src/main/java/com/studymate/app/MainActivity.java
@@ -1,5 +1,41 @@
 package com.studymate.app;
 
+import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+
+                WebView webView = getBridge().getWebView();
+
+                if (webView == null) {
+                    finishAndRemoveTask();
+                    return;
+                }
+
+                String url = webView.getUrl();
+
+                if (url != null && url.contains("dashboard.html")) {
+                    webView.clearHistory();
+                    finishAndRemoveTask();
+                    return;
+                }
+
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finishAndRemoveTask();
+                }
+            }
+        });
+    }
+}
